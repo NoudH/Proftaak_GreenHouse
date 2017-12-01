f364baec03eced547a540bf5fb7f14f06b2d3c8a
NoudH <noudhabraken@gmail.com>
2017-12-01 14:01:45 +0100

    Tabs on Tabcontrol
    Added and named tabs on the tabcontrol

--- Project Greenhouse/Project Greenhouse/Form1.Designer.cs
+++ Project Greenhouse/Project Greenhouse/Form1.Designer.cs
@@ -29,40 +29,62 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TabGraphs = new System.Windows.Forms.TabPage();
+            this.TabLiveFeed = new System.Windows.Forms.TabPage();
+            this.TabLogin = new System.Windows.Forms.TabPage();
+            this.tabLogs = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.TabLogin);
+            this.tabControl1.Controls.Add(this.TabGraphs);
+            this.tabControl1.Controls.Add(this.TabLiveFeed);
+            this.tabControl1.Controls.Add(this.tabLogs);
             this.tabControl1.Location = new System.Drawing.Point(0, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(813, 491);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // TabGraphs
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(805, 465);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.TabGraphs.Location = new System.Drawing.Point(4, 22);
+            this.TabGraphs.Name = "TabGraphs";
+            this.TabGraphs.Padding = new System.Windows.Forms.Padding(3);
+            this.TabGraphs.Size = new System.Drawing.Size(805, 465);
+            this.TabGraphs.TabIndex = 0;
+            this.TabGraphs.Text = "Graphs";
+            this.TabGraphs.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // TabLiveFeed
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.TabLiveFeed.Location = new System.Drawing.Point(4, 22);
+            this.TabLiveFeed.Name = "TabLiveFeed";
+            this.TabLiveFeed.Padding = new System.Windows.Forms.Padding(3);
+            this.TabLiveFeed.Size = new System.Drawing.Size(805, 465);
+            this.TabLiveFeed.TabIndex = 1;
+            this.TabLiveFeed.Text = "Live Feed";
+            this.TabLiveFeed.UseVisualStyleBackColor = true;
+            // 
+            // TabLogin
+            // 
+            this.TabLogin.Location = new System.Drawing.Point(4, 22);
+            this.TabLogin.Name = "TabLogin";
+            this.TabLogin.Size = new System.Drawing.Size(805, 465);
+            this.TabLogin.TabIndex = 2;
+            this.TabLogin.Text = "Login";
+            this.TabLogin.UseVisualStyleBackColor = true;
+            // 
+            // tabLogs
+            // 
+            this.tabLogs.Location = new System.Drawing.Point(4, 22);
+            this.tabLogs.Name = "tabLogs";
+            this.tabLogs.Size = new System.Drawing.Size(805, 465);
+            this.tabLogs.TabIndex = 3;
+            this.tabLogs.Text = "Logs";
+            this.tabLogs.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -80,8 +102,10 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage TabGraphs;
+        private System.Windows.Forms.TabPage TabLiveFeed;
+        private System.Windows.Forms.TabPage TabLogin;
+        private System.Windows.Forms.TabPage tabLogs;
     }
 }
 
