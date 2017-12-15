631022b4d35343fdcc0cf2b537ee275610469729
NoudH <noudhabraken@gmail.com>
2017-12-15 11:32:57 +0100

    Serial Messenger en Plant tab uitgewerkt
    Serial Messenger en Plant tab uitgewerkt.

--- Project Greenhouse/Project Greenhouse/Form1.cs
+++ Project Greenhouse/Project Greenhouse/Form1.cs
@@ -7,14 +7,107 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Project_Greenhouse
 {
     public partial class Form1 : Form
     {
+        private SerialMessenger serialMessenger;
+        List<Plant> plantenLijst = new List<Plant>();
+
         public Form1()
         {
             InitializeComponent();
+            ReadPlantFile();
+            
+
+            MessageBuilder messageBuilder = new MessageBuilder('#', '%');
+            serialMessenger = new SerialMessenger("COM4", 115200, messageBuilder);
+
+            //TODO: Zet dit in een button
+            //serialMessenger.Connect();            
+        }        
+
+        void ReadPlantFile()
+        {
+            if (File.Exists("PlantFile.txt"))
+            {
+                string[] data = File.ReadAllLines("PlantFile.txt");
+                for (int i = 0; i < data.Length; i+=3)
+                {
+                    Plant p = new Plant(data[i], int.Parse(data[i + 1]));
+                    plantenLijst.Add(p);
+                    LbPlanten.Items.Add(p.Naam);
+                }
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialMessenger.Disconnect();
+            }
+            catch 
+            {
+
+            }            
+        }
+
+        private void WriteLog(string _message)
+        {
+            TxtLogging.Text += String.Format("[{0}] {1}\n", DateTime.Now.ToString(), _message);
+        }
+
+        private void BtnAddPlant_Click(object sender, EventArgs e)
+        {
+            Plant p = new Plant(TxtPlantnaam.Text, (int)NudWater.Value);
+            plantenLijst.Add(p);
+            LbPlanten.Items.Add(p.Naam);
+
+            File.AppendAllLines("PlantFile.txt", new string[] { String.Format("{0}\n{1}\n", TxtPlantnaam.Text, (int)NudWater.Value) });
+        }
+
+        private void LbPlanten_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                TxtPlantnaam.Text = FindPlant(LbPlanten.SelectedItem.ToString()).Naam;
+                NudWater.Value = FindPlant(LbPlanten.SelectedItem.ToString()).Water;
+            }
+            catch 
+            {
+
+            }            
+        }
+
+        private Plant FindPlant(string _naam)
+        {
+            return plantenLijst.Find(delegate (Plant p)
+            {
+                return p.Naam == _naam;
+            });
+        }
+
+        private void BtnSendPlant_Click(object sender, EventArgs e)
+        {
+            serialMessenger.Connect();
+            serialMessenger.SendMessage(FindPlant(LbPlanten.SelectedItem.ToString()).Water.ToString());
+        }
+
+        private void BtnRemovePlant_Click(object sender, EventArgs e)
+        {
+            List<string> FileContent = new List<string>();
+            FileContent = File.ReadAllLines("PlantFile.txt").ToList();
+            int index = FileContent.IndexOf(LbPlanten.SelectedItem.ToString());
+
+            FileContent.RemoveRange(index, 3);
+
+            File.WriteAllLines("PlantFile.txt", FileContent);
+            plantenLijst.Remove(FindPlant(LbPlanten.SelectedItem.ToString()));
+            LbPlanten.Items.RemoveAt(LbPlanten.SelectedIndex);
         }
     }
 }
---
+++ Project Greenhouse/Project Greenhouse/Plant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Greenhouse
+{
+    class Plant
+    {
+        private string naam;
+        private int water;
+
+        public Plant(string _naam, int _water)
+        {
+            naam = _naam;
+            water = _water;
+        }
+
+        public string Naam
+        {
+            get { return naam; }
+        }
+
+        public int Water
+        {
+            get { return water; }
+        }
+    }
+}
